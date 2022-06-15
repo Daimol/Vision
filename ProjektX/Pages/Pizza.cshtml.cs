1b60c884ea86ced571dd982cec3d3b166fbaf0bc
Lukys <martinec-lukas@seznam.cz>
2022-06-15 07:20:18 +0200

    push

---
+++ ProjektX/Pages/Pizza.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektX.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+    }
+}
